baa6a9e1c6b4993f7741bbac8d9ca768c4b51640
Ravi Kumar Gupta <ravikumarg05082004@gmail.com>
2026-04-17 16:38:10 +0530

    Profile Image updated

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -22,7 +22,7 @@ export default function HeroSection() {
             transition={{ ...scaleIn.transition, delay: 0.2 }}
           >
             <Image
-              src="/profile.jpeg"
+              src="/Profileimg17apr.jpeg"
               alt="Profile"
               width={100}
               height={100}
